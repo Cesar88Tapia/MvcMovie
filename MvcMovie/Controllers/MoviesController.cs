d313a2ac0f401bb3db6cf465d2dee985569dba76
César Tapia <c.tapia@korvi.net>
2018-10-15 14:02:56 -0500

    Fixed server error

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -19,7 +19,7 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string MovieGenre, string searchString)
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
 
             IQueryable<string> genreQuery = from m in _context.Movie
@@ -36,19 +36,25 @@ namespace MvcMovie.Controllers
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
 
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
             var movieGenreVM = new MovieGenreViewModel();
             movieGenreVM.genres = new SelectList(await genreQuery.Distinct().ToListAsync());
             movieGenreVM.movies = await movies.ToListAsync();
 
-            return View(await _context.Movie.ToListAsync());
+            //return View(await _context.Movie.ToListAsync());
+            return View(movieGenreVM);
         }
 
         //Post: Movies
-        //[HttpPost]
-        //public string Index(string seachString, bool notUsed)
-        //{
-        //    return "From [HttpPost] Index: filter on " + seachString;
-        //}
+        [HttpPost]
+        public string Index(string seachString, bool notUsed)
+        {
+            return "From [HttpPost] Index: filter on " + seachString;
+        }
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
